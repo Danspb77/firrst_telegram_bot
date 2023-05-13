f3e83d775c8c276db3f6d51ffcd3cf2f4c8bf253
Danya <dan.shell@bk.ru>
2023-05-13 11:45:43 +0300

    working vres with urls, buttons

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import telebot
-from telebot import *
+from telebot import types
 
 bot=telebot.TeleBot("6072034229:AAE2PES80kdX5QNOzEdWzAPjUQW2v526Q6c")
 
@@ -7,6 +7,23 @@ bot=telebot.TeleBot("6072034229:AAE2PES80kdX5QNOzEdWzAPjUQW2v526Q6c")
 def start(message):
     text=f"hello, <b>{message.from_user.first_name}</b> <u>{message.from_user.first_name}</u>"
     bot.send_message(message.chat.id, text,parse_mode='html')
+
+@bot.message_handler(commands=["website"])
+
+def website(message):
+    markup=types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("visit the website", url= "https://www.cyberforum.ru/python/thread2007924.html?ysclid=lhlp73h4pa582816413"))
+    bot.send_message(message.chat.id,"go on the website", reply_markup=markup)
+
+@bot.message_handler(commands=["help"])
+
+def website_in_field(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=1)
+    website=types.KeyboardButton("web site")
+    start=types.KeyboardButton("start")
+    markup.add(website,start)
+    bot.send_message(message.chat.id,"go on the website", reply_markup=markup)
+
 
 @bot.message_handler()
 
@@ -27,6 +44,8 @@ def get_user_text(message):
 
 def get_user_photo(message):
     bot.send_message(message.chat.id, "looks nice")
-   
+
+
+
 
 bot.polling(none_stop=True)
